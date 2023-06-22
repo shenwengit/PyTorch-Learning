751a7af123f96395abb7b37248260f8d7de55923
SHENWEN\Administrator <592124318@qq.com>
2023-06-22 16:07:17 +0800

    linear regression implemented on torch

--- linear_regression.py
+++ linear_regression.py
@@ -1,7 +1,11 @@
 import torch
 import random
+import numpy as np
 import matplotlib.pyplot as plt
 from d2l import torch as d2l
+from torch.utils import data
+from torch import nn
+
 
 def synthetic_data(w, b, num_examples):
     # 生成 y = w*x + b
@@ -67,8 +71,46 @@ def train_lin():
     print(f'b训练后的误差：{true_b - b}')
 
 
+###################linear regression using nn#####################
+def load_array(data_array, batch_size, train = True):
+    '''数据迭代器'''
+    dataset = data.TensorDataset(*data_array)
+    return data.DataLoader(dataset, batch_size, shuffle = train)
+
+def train_lin_nn():
+    true_w = torch.tensor([3, -5.6])
+    true_b = 4.2
+    lr = 0.03
+    num_epochs = 3
+    batch_size = 10
+
+    features, lables = d2l.synthetic_data(true_w, true_b, 1000)
+    data_iter = load_array((features, lables), batch_size)
+
+    net = nn.Sequential(nn.Linear(2, 1))
+    net[0].weight.data.normal_(0, 0.01)
+    net[0].bias.data.fill_(0)
+
+    loss = nn.MSELoss()
+    trainer = torch.optim.SGD(net.parameters(), lr = lr)
+
+    for epoch in range(num_epochs):
+        for X, y in data_iter:
+            l = loss(net(X), y)
+            trainer.zero_grad()
+            l.backward()
+            trainer.step()
+        l = loss(net(features), lables)
+        print(f'epoch {epoch + 1}, loss {l:f}')
+    
+    print(f'w训练后的误差：{true_w - net[0].weight.reshape(true_w.shape)}')
+    print(f'b训练后的误差：{true_b - net[0].bias}')
+
+
+    
 
 
 if __name__ == '__main__':
-    train_lin()
+    # train_lin()
+    train_lin_nn()
         
